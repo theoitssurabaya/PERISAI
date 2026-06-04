56bd490686fe467cb4aed3dfd41c6253dedb9dfc
Hilmy Abid Syafi Abiyyu <hilmyabidhasa@gmail.com>
2026-06-04 11:03:47 +0700

    add corsoptions

--- Fullstack/Backend/src/server.js
+++ Fullstack/Backend/src/server.js
@@ -11,6 +11,14 @@ const app = express();
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ['https://perisai-ptm.vercel.app/', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(cors());
 app.use(express.json());
 
@@ -26,7 +34,6 @@ app.use('/api/health-records', require('./routes/healthRecord'));
 app.use('/api/profile', profileRoutes)
 app.use('/api/prediction', predictionRoutes)
 app.use('/api/chat', require('./routes/chat'))
-app.use('/api/health-records', require('./routes/healthRecord'))
 
 app.use(errorHandler);
 
