f9d77fe11ee908a9965ccb453456813f19f98663
Hilmy Abid Syafi Abiyyu <hilmyabidhasa@gmail.com>
2026-06-04 22:05:01 +0700

    fix origin from localhost to vercel

--- Fullstack/Backend/src/server.js
+++ Fullstack/Backend/src/server.js
@@ -12,7 +12,7 @@ const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['https://perisai-ptm.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
