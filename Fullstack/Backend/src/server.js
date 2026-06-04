d070a472201cc68922e73b59c642b6630d06328d
Hilmy Abid Syafi Abiyyu <hilmyabidhasa@gmail.com>
2026-06-04 12:51:55 +0700

    fix corsOptions

--- Fullstack/Backend/src/server.js
+++ Fullstack/Backend/src/server.js
@@ -12,14 +12,13 @@ const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ['https://perisai-ptm.vercel.app/', 'http://localhost:5173'],
+  origin: ['https://perisai-ptm.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
 app.use(cors(corsOptions));
-
-app.use(cors());
 app.use(express.json());
 
 // Health check
